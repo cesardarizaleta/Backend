45323bb391526457e8921ece7354360aa74f08ae
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-09-03 22:02:15 -0400

    Conexion Lista y Probada

--- first-connection/js/conexion.js
+++ first-connection/js/conexion.js
@@ -2,7 +2,34 @@ let mysql = require('mysql');
 
 let connection = mysql.createConnection({
     host: 'localhost',
-    user: '',
-    password: '',
-    database: 'first_connection'
+    user: 'root',
+    password: 'password',
+    database: 'sys'
 });
+
+connection.connect(function(error){
+    if(error){
+        throw error;
+    }else{
+        console.log('Conexion correcta');
+    }
+})
+
+let registro = {name: 'Cesar', apellido: 'Dominguez'};
+let sqlINSERT = 'INSERT INTO registro SET ?';
+connection.query(sqlINSERT, registro, function(error, result){
+    if(error){
+        throw error;
+    }
+});
+
+let sql = 'SELECT * FROM registro';
+connection.query(sql, function(error, result){
+    if(error){
+        throw error;
+    }else{
+        console.log(result);
+    }
+});
+
+connection.end();
