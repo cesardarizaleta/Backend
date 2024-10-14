348578b37d076618bc7fd999bd7ec4ab509bf680
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-10-14 00:11:25 -0400

    create modules

--- api/src/DB/mysql.js
+++ api/src/DB/mysql.js
@@ -1,13 +1,40 @@
 const mysql = require('mysql')
 const config = require('../config')
 
-const prueba = {
-    name: 'Cesar',
-    apellido: 'Novedoso'
+const dbconfig = {
+    host: config.mysql.host,
+    user: config.mysql.user,
+    password: config.mysql.password,
+    database: config.mysql.database
 }
 
+let connection;
+
+function connectMYSQL() {
+    connection = mysql.createConnection(dbconfig)
+
+    connection.connect((err) => {
+        if (err) {
+            console.error('Error connecting to MYSQL: ' + err.stack)
+            setTimeout(connectMYSQL,2000)
+        }
+        console.log('Connected to MYSQL as id ' + connection.threadId)
+    })
+
+    connection.on('error', (err) => {
+        console.error('MYSQL error: ' + err)
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            connectMYSQL()
+        } else {
+            throw err
+        }
+    })
+}
+
+connectMYSQL()
+
 function datos(tabla) {
-    return prueba
+    return tabla
 }
 
 function dato(tabla,id) {
